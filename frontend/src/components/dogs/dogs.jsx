54cd6f124c930bb2ff504129eaf5ab1375e96946
Yaswanthram-396 <yaswanthram.396@gmail.com>
2026-08-31 20:52:34 +0530

    formated

--- frontend/src/components/dogs/dogs.jsx
+++ frontend/src/components/dogs/dogs.jsx
@@ -1,55 +1,41 @@
+import React, { useState, useEffect } from "react";
 
-import React,{useState,useEffect} from 'react'
+function Dogs() {
+  const [dogs, setDogs] = useState([]);
+  const [load, setLoad] = useState(false);
 
-function Dogs(){
-    const [dogs,setDogs]=useState([]);
-    const [load,setLoad]=useState(false);
+  useEffect(() => {
+    getDogs();
+  }, []);
 
-    useEffect(()=>{
-        
-        getDogs();
-    },[])
-
-    const getDogs=async()=>{
-        setLoad(true);
-        try{
-            const data=await fetch('https://dog.ceo/api/breed/hound/images');
-            const response=await data.json();
-            setDogs(response.message);
-        }
-        catch(e){
-            
-            alert("Pleaase Try Again Later")
-        }
-        finally{
-
-            setLoad(false);
-        }
+  const getDogs = async () => {
+    setLoad(true);
+    try {
+      const data = await fetch("https://dog.ceo/api/breed/hound/images");
+      const response = await data.json();
+      setDogs(response.message);
+    } catch (e) {
+      alert("Pleaase Try Again Later");
+    } finally {
+      setLoad(false);
     }
+  };
 
-    const showdata=(val)=>{
-        return(
-            val.map((dog)=>(
-            <>
-                <div>
-                    <img src={dog}/>
-                </div>
-            </>
-        ))
-        )
-    }
-    
-    return(
-        <>
-        {
-            load?
-            (<>Loading...</>):
-            dogs.length>0?
-            showdata(dogs)
-            :<>No Data</>
-        }
-        </>
-    )
+  const showdata = (val) => {
+    return val.map((dog) => (
+      <>
+        <div>
+          <img src={dog} />
+        </div>
+      </>
+    ));
+  };
+
+  return (
+    <>
+      {load ? <>Loading...</> : dogs.length > 0 ? showdata(dogs) : <>No Data</>}
+    </>
+  );
 }
 
 export default Dogs;
